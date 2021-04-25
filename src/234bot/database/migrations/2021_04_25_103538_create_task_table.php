f110aa9ca5760ee059dc9cf2e848c760740eea2e
arsa-dg <13519037@std.stei.itb.ac.id>
2021-04-25 18:03:23 +0700

    migration: add task and kata_penting table

---
+++ src/234bot/database/migrations/2021_04_25_103538_create_task_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamp('deadline')->nullable();
+            $table->string('mata_kuliah');
+            $table->string('jenis_tugas');
+            $table->foreign('jenis_tugas')->references('kata_penting')->on('kata_penting');
+            $table->string('topik');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task');
+    }
+}
