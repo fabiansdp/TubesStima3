9e2eaf6d948a11efd622892f45ae16a511d158a7
nypnop <68521696+nypnop@users.noreply.github.com>
2021-04-27 21:40:06 +0700

    add Stringmatching KMP

--- src/234bot/app/Http/Controllers/TaskController.php
+++ src/234bot/app/Http/Controllers/TaskController.php
@@ -22,4 +22,78 @@ class TaskController extends Controller
         $task->save();
         return redirect('/');
     }
+    
+    function KMPSearch($pat, $txt)
+    {
+        $M = strlen($pat);
+        $N = strlen($txt);
+
+        // create lps[] that will hold the longest prefix suffix
+        // values for pattern
+        $lps=array_fill(0,$M,0);
+
+        // Preprocess the pattern (calculate lps[] array)
+        computeLPSArray($pat, $M, $lps);
+
+        $i = 0; // index for txt[]
+        $j = 0; // index for pat[]
+        while ($i < $N) {
+            if ($pat[$j] == $txt[$i]) {
+                $j++;
+                $i++;
+            }
+
+            if ($j == $M) {
+                return true;
+                $j = $lps[$j - 1];
+            }
+
+            // mismatch after j matches
+            else if ($i < $N && $pat[$j] != $txt[$i]) {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if ($j != 0)
+                    $j = $lps[$j - 1];
+                else
+                    $i++;
+            }
+        }
+        return false;
+    }
+
+    // Fills lps[] for given patttern pat[0..M-1]
+    function computeLPSArray($pat, $M, &$lps)
+    {
+        // length of the previous longest prefix suffix
+        $len = 0;
+
+        $lps[0] = 0; // lps[0] is always 0
+
+        // the loop calculates lps[i] for i = 1 to M-1
+        $i = 1;
+        while ($i < $M) {
+            if ($pat[$i] == $pat[$len]) {
+                $len++;
+                $lps[$i] = $len;
+                $i++;
+            }
+            else // (pat[i] != pat[len])
+            {
+                // This is tricky. Consider the example.
+                // AAACAAAA and i = 7. The idea is similar
+                // to search step.
+                if ($len != 0) {
+                    $len = $lps[$len - 1];
+
+                    // Also, note that we do not increment
+                    // i here
+                }
+                else // if (len == 0)
+                {
+                    $lps[$i] = 0;
+                    $i++;
+                }
+            }
+        }
+    }
 }
